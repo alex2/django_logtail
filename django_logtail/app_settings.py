8e3e5ac10beee7db089be3477932813887384b13
Alex Holmes <alex@alex-holmes.com>
2012-04-22 15:10:49 +0100

    Empty by default.

--- django_logtail/app_settings.py
+++ django_logtail/app_settings.py
@@ -1,4 +1,4 @@
 from django.conf import settings
 
-LOGTAIL_FILES = getattr(settings, 'LOGTAIL_FILES', {'apache': '/var/log/apache2/access.log'})
+LOGTAIL_FILES = getattr(settings, 'LOGTAIL_FILES', {})
 LOGTAIL_INCLUDE_JQUERY = getattr(settings, 'LOGTAIL_INCLUDE_JQUERY', True)
