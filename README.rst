f996c1f2f3218bda8c4bb5f565bc66f1bd8b16a1
Alex Holmes <alex.holmes@isotoma.com>
2013-12-11 14:34:38 +0000

    Update example project to Django 1.6

--- README.rst
+++ README.rst
@@ -9,7 +9,7 @@ Logtail allows you to view logfiles for your django project via your django
 admin, and will also provide you with a live tail of the logfiles using ajax
 polling - and therefore without the need for a long-running django process.
 
-Django 1.3+ compatible.
+Django 1.4+ compatible.
 
 See Also: https://pypi.python.org/pypi/django_logtail
 
--- example/settings.py
+++ example/settings.py
@@ -1,59 +1,87 @@
+"""
+Django settings for example project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 LOGTAIL_FILES = {
-    'noexist': '/foo/bar',
-    'test': 'test.log',
+    'noexist': '/django_logtail_noexist',
+    'test': os.path.join(BASE_DIR, 'test.log'),
 }
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '3gdby#ik#c(t+nv0b+i*o^=vjn0*tg7vm4q)em!m!k87n6%c#='
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'example.db',
-    }
-}
-SITE_ID = 1
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
 
-STATIC_ROOT = ''
-STATIC_URL = '/static/'
+TEMPLATE_DEBUG = True
 
-SECRET_KEY = ')@g^2o!ojviexmcsbr%pfctj!2fx-v7=c*rn$7(*k%y8u!!)o0'
-ROOT_URLCONF = 'example.urls'
-WSGI_APPLICATION = 'example.wsgi.application'
+ALLOWED_HOSTS = []
+
+
+# Application definition
 
 INSTALLED_APPS = (
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
     'django_logtail',
 )
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'example.urls'
+
+WSGI_APPLICATION = 'example.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
